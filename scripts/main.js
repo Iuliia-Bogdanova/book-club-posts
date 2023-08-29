c10a8fde6a996e7239eae429bc82884686564ff5
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-08-29 22:37:35 +0400

    looking for faults

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,110 @@
-const apiPosts = "https://dummyjson.com/post";
+const apiPosts = "https://dummyjson.com/posts";
 
 const cards = document.querySelector(".success-post");
+const errorPost = document.querySelector(".error-post");
+let newCard = "";
 
-// получаем данные с API в консоль и в карточку undefined
+// 1. функция работает при "https://dummyjson.com/posts/1" - выводится в 1 карточке 1 пост. при "https://dummyjson.com/posts" - в карточке отрисовывается undefined. вся разметка в HTML
+
 function getPosts() {
     fetch(apiPosts)
         .then((res) => res.json())
         .then((posts) => {
-            console.log(posts);
-            newCard = posts;
-            const body = posts.body;
-            cards.innerHTML = body;
+        console.log(posts);
+        newCard = posts;
+        const body = posts.body;
+        cards.innerHTML = body;
         })
-    .catch((error) => {
+        .catch((error) => {
         console.error(error);
     });
 }
 
 getPosts();
 
+// 2. то же самое, только разметка в JS - если убираю из HTML - пост не отрисовывается. при "https://dummyjson.com/posts" - в карточке пусто
+// (методы DOM для создания и добавления элементов в HTML-структуру, а также методы для установки классов и текстового содержимого элементов)
+
+// function getPosts() {
+//     fetch(apiPosts)
+//         .then((res) => res.json())
+//         .then((posts) => {
+//         console.log(posts);
+//         newCard = posts;
+//         const body = posts.body;
+//         const newCardElement = document.createElement("p");
+//         newCardElement.classList.add("body-post");
+//         newCardElement.textContent = body;
+//         cards.appendChild(newCardElement);
+//         })
+//         .catch((error) => {
+//         console.error(error);
+//     });
+// }
+
+// getPosts();
+
+// 3. forEach, динамическая разметка DOM. карточки не отрисовываются
+
+// function getPosts() {
+//     fetch(apiPosts)
+//         .then((res) => res.json())
+//         .then((posts) => {
+//         console.log(posts);
+//         if (Array.isArray(posts)) {
+//             posts.forEach((post) => {
+//             const successPost = document.createElement("div");
+//             successPost.classList.add("success-post");
+
+//             const titlePost = document.createElement("p");
+//             titlePost.classList.add("title-post");
+//             titlePost.textContent = post.body;
+
+//             successPost.appendChild(titlePost);
+//             cards.appendChild(successPost);
+//             });
+//         } else {
+//             errorPost.style.display = "block";
+//         }
+//         })
+//         .catch((error) => {
+//         console.error(error);
+//     });
+// }
+
+// getPosts();
+
+// 4. используем forEach, переносим разметку шаблонными строками, карточки не отрисовываются
+
+// function getPosts() {
+//     fetch(apiPosts)
+//         .then((res) => res.json())
+//         .then((posts) => {
+//         if (Array.isArray(posts)) {
+//             posts.forEach((post) => {
+//             newCard += `
+//                 <div class="success-post">
+//                 <p class="title-post">${post.body}</p>
+//                 </div>
+//             `;
+//             });
+//             cards.innerHTML = newCard;
+//         } else {
+//             errorPost.style.display = "block";
+//         }
+//         console.log(posts);
+//         })
+//         .catch((error) => {
+//         console.log(error);
+//         errorPost.style.display = "block";
+//     });
+// }
+
+// getPosts();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const cards = document.querySelector(".posts-form");
-// const errorPost = document.querySelector(".error-post");
-// let newCard = "";
+// 5. использую map, шаблонные строки, карточки не отрисовываются
 
 // function getPosts() {
 //     fetch(apiPosts)
@@ -48,11 +115,9 @@ getPosts();
 //             const body = posts.body
 //             .map((post) => {
 //                 return (newCard += `
-//                 <div class="posts-form">
-//                         <div class="success-post">
-//                             <p class="title-post">${post.body}</p>
+//                     <div class="success-post">
+//                         <p class="title-post">${posts.body}</p>
 //                         </div>
-//                     </div>
 //                     `);
 //             })
 //             .join("");
@@ -69,3 +134,7 @@ getPosts();
 // }
 
 // getPosts();
+
+
+
+
