587456b81016dce3daf36f8fb69590ea89779977
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-09-11 00:55:40 +0400

    geting elements in cards

--- scripts/main.js
+++ scripts/main.js
@@ -4,23 +4,109 @@ const cards = document.querySelector(".success-post");
 const errorPost = document.querySelector(".error-post");
 let newCard = "";
 
-// 1. функция работает при "https://dummyjson.com/posts/1" - выводится в 1 карточке 1 пост. при "https://dummyjson.com/posts" - в карточке отрисовывается undefined. вся разметка в HTML
+// функции для создания элементов разметки
+function createNode(element) {
+  return document.createElement(element);
+}
 
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+// получаем и отрисовываем элементы
 function getPosts() {
-    fetch(apiPosts)
-        .then((res) => res.json())
-        .then((posts) => {
-        console.log(posts);
-        newCard = posts;
-        const body = posts.body;
-        cards.innerHTML = body;
-        })
-        .catch((error) => {
-        console.error(error);
+  fetch(apiPosts)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      console.log(data);
+
+      // Проверяем, есть ли элементы
+      if (data.posts && data.posts.length > 0) {
+        data.posts.forEach(function (post) {
+          // Создаем контейнер для карточки
+          const card = createNode("div");
+          card.classList.add("card");
+
+          // Создаем заголовок карточки
+          const title = createNode("h3");
+          title.innerHTML = post.title;
+          append(card, title);
+
+          // Создаем содержимое карточки
+          const content = createNode("p");
+          content.innerHTML = post.body;
+          append(card, content);
+
+          append(cards, card);
+        });
+      } else {
+        // Если элементы не найдены, отображаем сообщение об ошибке
+        const errorMessage = createNode("p");
+        errorMessage.classList.add("error-message");
+        errorMessage.innerHTML = "No posts found";
+        append(errorPost, errorMessage);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
     });
 }
 
 getPosts();
+
+// const apiPosts = "https://dummyjson.com/posts";
+
+// const cards = document.querySelector(".success-post");
+// const errorPost = document.querySelector(".error-post");
+// let newCard = "";
+
+// // функции для создания элементов разметки
+// function createNode(element) {
+//     return document.createElement(element);
+//     }
+
+//     function append(parent, el) {
+//     return parent.appendChild(el);
+// }
+
+// // получаем и отрисовываем элементы
+// function getPosts() {
+//     fetch(apiPosts)
+//     .then((resp) => resp.json())
+//     .then(function(data) {
+//         console.log(data);
+//         let posts = data.posts || [];
+//         return posts.forEach(function(post) {
+//             let p = createNode("p");
+//             p.innerHTML = `${post.id}`;
+//             append(cards, p);
+//         })
+        
+//     })
+//     .catch(function(error) {
+//         console.log(error);
+//     })
+// }
+
+// getPosts();
+
+// 1. функция работает при "https://dummyjson.com/posts/1" - выводится в 1 карточке 1 пост. при "https://dummyjson.com/posts" - в карточке отрисовывается undefined. вся разметка в HTML
+
+// function getPosts() {
+//     fetch(apiPosts)
+//         .then((res) => res.json())
+//         .then((posts) => {
+//         console.log(posts);
+//         newCard = posts;
+//         const body = posts.body;
+//         cards.innerHTML = body;
+//         })
+//         .catch((error) => {
+//         console.error(error);
+//     });
+// }
+
+// getPosts();
 
 // 2. то же самое, только разметка в JS - если убираю из HTML - пост не отрисовывается. при "https://dummyjson.com/posts" - в карточке пусто
 // (методы DOM для создания и добавления элементов в HTML-структуру, а также методы для установки классов и текстового содержимого элементов)
