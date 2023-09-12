9829d0fb556f670a1a122eaa8d52469d429a8d54
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-09-12 20:15:57 +0400

    add cards & function deletePosts

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 const apiPosts = "https://dummyjson.com/posts";
-
-const cards = document.querySelector(".success-post");
+const cards = document.querySelector(".posts-form");
 const errorPost = document.querySelector(".error-post");
-let newCard = "";
+
+const deleteButton = document.querySelector(".icon-post-del");
+deleteButton.addEventListener("click", deletePosts);
+
+// функция для удаления всех постов
+function deletePosts() {
+  cards.innerHTML = "";
+}
 
 // функции для создания элементов разметки
 function createNode(element) {
@@ -32,6 +38,11 @@ function getPosts() {
           title.innerHTML = post.title;
           append(card, title);
 
+          // Создаем список тегов
+          const tags = createNode("h4");
+          tags.innerHTML = post.tags;
+          append(card, tags);
+
           // Создаем содержимое карточки
           const content = createNode("p");
           content.innerHTML = post.body;
@@ -53,174 +64,3 @@ function getPosts() {
 }
 
 getPosts();
-
-// const apiPosts = "https://dummyjson.com/posts";
-
-// const cards = document.querySelector(".success-post");
-// const errorPost = document.querySelector(".error-post");
-// let newCard = "";
-
-// // функции для создания элементов разметки
-// function createNode(element) {
-//     return document.createElement(element);
-//     }
-
-//     function append(parent, el) {
-//     return parent.appendChild(el);
-// }
-
-// // получаем и отрисовываем элементы
-// function getPosts() {
-//     fetch(apiPosts)
-//     .then((resp) => resp.json())
-//     .then(function(data) {
-//         console.log(data);
-//         let posts = data.posts || [];
-//         return posts.forEach(function(post) {
-//             let p = createNode("p");
-//             p.innerHTML = `${post.id}`;
-//             append(cards, p);
-//         })
-        
-//     })
-//     .catch(function(error) {
-//         console.log(error);
-//     })
-// }
-
-// getPosts();
-
-// 1. функция работает при "https://dummyjson.com/posts/1" - выводится в 1 карточке 1 пост. при "https://dummyjson.com/posts" - в карточке отрисовывается undefined. вся разметка в HTML
-
-// function getPosts() {
-//     fetch(apiPosts)
-//         .then((res) => res.json())
-//         .then((posts) => {
-//         console.log(posts);
-//         newCard = posts;
-//         const body = posts.body;
-//         cards.innerHTML = body;
-//         })
-//         .catch((error) => {
-//         console.error(error);
-//     });
-// }
-
-// getPosts();
-
-// 2. то же самое, только разметка в JS - если убираю из HTML - пост не отрисовывается. при "https://dummyjson.com/posts" - в карточке пусто
-// (методы DOM для создания и добавления элементов в HTML-структуру, а также методы для установки классов и текстового содержимого элементов)
-
-// function getPosts() {
-//     fetch(apiPosts)
-//         .then((res) => res.json())
-//         .then((posts) => {
-//         console.log(posts);
-//         newCard = posts;
-//         const body = posts.body;
-//         const newCardElement = document.createElement("p");
-//         newCardElement.classList.add("body-post");
-//         newCardElement.textContent = body;
-//         cards.appendChild(newCardElement);
-//         })
-//         .catch((error) => {
-//         console.error(error);
-//     });
-// }
-
-// getPosts();
-
-// 3. forEach, динамическая разметка DOM. карточки не отрисовываются
-
-// function getPosts() {
-//     fetch(apiPosts)
-//         .then((res) => res.json())
-//         .then((posts) => {
-//         console.log(posts);
-//         if (Array.isArray(posts)) {
-//             posts.forEach((post) => {
-//             const successPost = document.createElement("div");
-//             successPost.classList.add("success-post");
-
-//             const titlePost = document.createElement("p");
-//             titlePost.classList.add("title-post");
-//             titlePost.textContent = post.body;
-
-//             successPost.appendChild(titlePost);
-//             cards.appendChild(successPost);
-//             });
-//         } else {
-//             errorPost.style.display = "block";
-//         }
-//         })
-//         .catch((error) => {
-//         console.error(error);
-//     });
-// }
-
-// getPosts();
-
-// 4. используем forEach, переносим разметку шаблонными строками, карточки не отрисовываются
-
-// function getPosts() {
-//     fetch(apiPosts)
-//         .then((res) => res.json())
-//         .then((posts) => {
-//         if (Array.isArray(posts)) {
-//             posts.forEach((post) => {
-//             newCard += `
-//                 <div class="success-post">
-//                 <p class="title-post">${post.body}</p>
-//                 </div>
-//             `;
-//             });
-//             cards.innerHTML = newCard;
-//         } else {
-//             errorPost.style.display = "block";
-//         }
-//         console.log(posts);
-//         })
-//         .catch((error) => {
-//         console.log(error);
-//         errorPost.style.display = "block";
-//     });
-// }
-
-// getPosts();
-
-
-
-// 5. использую map, шаблонные строки, карточки не отрисовываются
-
-// function getPosts() {
-//     fetch(apiPosts)
-//         .then((res) => res.json())
-//         .then((posts) => {
-//         if (Array.isArray(posts)) {
-//             newCard = posts
-//             const body = posts.body
-//             .map((post) => {
-//                 return (newCard += `
-//                     <div class="success-post">
-//                         <p class="title-post">${posts.body}</p>
-//                         </div>
-//                     `);
-//             })
-//             .join("");
-//             cards.innerHTML = newCard;
-//         } else {
-//             errorPost.style.display = "block";
-//         }
-//         console.log(posts);
-//         })
-//         .catch((error) => {
-//         console.log(error);
-//         errorPost.style.display = "block";
-//     });
-// }
-
-// getPosts();
-
-
-
-
