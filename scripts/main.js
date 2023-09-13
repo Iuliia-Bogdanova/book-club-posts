0627d233b1769981dcb352e3f1e805c5f413c65e
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-09-13 13:56:37 +0400

    remained observer

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const apiPosts = "https://dummyjson.com/posts";
+const apiPosts = "https://dummyjson.com/posts?limit=150&skip=0&select=title,tags,body";
 const cards = document.querySelector(".posts-form");
 const errorPost = document.querySelector(".error-post");
 const searchButton = document.querySelector(".icon-post-search");
@@ -7,14 +7,14 @@ const deleteButton = document.querySelector(".icon-post-del");
 deleteButton.addEventListener("click", deletePosts);
 const input = document.querySelector(".input");
 
-// функция для удаления всех постов
+// удаляем все посты
 function deletePosts() {
   cards.innerHTML = "";
   input.value = "";
   errorPost.innerHTML = "";
 }
 
-// функции для создания элементов разметки
+// создаем элементы разметки
 function createNode(element) {
   return document.createElement(element);
 }
@@ -25,56 +25,62 @@ function append(parent, el) {
 
 // получаем и отрисовываем элементы
 function getPosts() {
-  
   const tag = input.value.toLowerCase().trim();
 
-  fetch(apiPosts)
-    .then((resp) => resp.json())
-    .then(function (data) {
-      console.log(data);
+  // проверяем, есть ли значение в input
+  if (tag) {
+    fetch(apiPosts)
+      .then((resp) => resp.json())
+      .then(function (data) {
+        console.log(data);
 
-      // Фильтруем посты по введенному тегу
-      const filteredPosts = data.posts.filter((post) =>
-        post.tags.includes(tag));
+        // фильтруем посты по keyword
+        const filteredPosts = data.posts.filter((post) =>
+          post.tags.includes(tag)
+        );
 
         console.log(filteredPosts);
 
-      // Проверяем, есть ли элементы
-      if (filteredPosts.length > 0) {
-        filteredPosts.forEach(function (post) {
-          // Создаем контейнер для карточки
-          const card = createNode("div");
-          card.classList.add("card");
+        // удаляем предыдущие сообщения об ошибке
+        errorPost.innerHTML = "";
 
-          // Создаем заголовок карточки
-          const title = createNode("h3");
-          title.innerHTML = post.title;
-          append(card, title);
+        // проверяем, есть ли посты по тегу
+        if (filteredPosts.length > 0) {
+          filteredPosts.forEach(function (post) {
+            // контейнер для карточки
+            const card = createNode("div");
+            card.classList.add("card");
 
-          // Создаем список тегов
-          const tags = createNode("h4");
-          tags.innerHTML = post.tags;
-          append(card, tags);
+            // заголовок карточки
+            const title = createNode("h3");
+            title.innerHTML = post.title;
+            append(card, title);
 
-          // Создаем содержимое карточки
-          const content = createNode("p");
-          content.innerHTML = post.body;
-          append(card, content);
+            // список тегов
+            const tags = createNode("h4");
+            tags.innerHTML = post.tags;
+            append(card, tags);
 
-          append(cards, card);
-        });
-        
-      } else {
-        // Если элементы не найдены, отображаем сообщение об ошибке
-        const errorMessage = createNode("h3");
-        errorMessage.classList.add("error-message");
-        errorMessage.innerHTML = "No posts found";
-        append(errorPost, errorMessage);
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+            // содержимое карточки
+            const content = createNode("p");
+            content.innerHTML = post.body;
+            append(card, content);
+
+            append(cards, card);
+          });
+        } else {
+          // если посты не найдены, выводим сообщение об ошибке
+          const errorMessage = createNode("h3");
+          errorMessage.classList.add("error-message");
+          errorMessage.innerHTML =
+            "sorry, No posts found. <br /><br />Please try using keywords such as:<br /><br />'american' 'english' 'french'<br />'classic' 'history' 'fiction'<br />'mystery' 'magical'<br />'love', 'crime'";
+          append(errorPost, errorMessage);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 }
 
 getPosts();
